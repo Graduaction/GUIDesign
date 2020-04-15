5fef5ae5fc1712c3151b6987f4068f1a286ba874
986140373 <986140373@qq.com>
2020-04-15 14:49:08 +0800

    界面更新

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -21,7 +21,7 @@ namespace GUI.UI
 
           //  Application.Run(new CheckStudentsVolunteer());
             //  Application.Run(new StudentForm());//  LoginInterface  NoticeDetails  DCheckNotification
-            Application.Run(new StudentForm());
+            Application.Run(new LoginInterface());
 
         }
     }
