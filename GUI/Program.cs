a7137a77a9d7385a615d06b0d5fe646a256086d5
986140373 <986140373@qq.com>
2020-04-17 15:40:13 +0800

    界面更新

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -15,7 +15,7 @@ namespace GUI.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());//  LoginInterface  NoticeDetails  DCheckNotification
+            Application.Run(new LoginInterface());//  LoginInterface  NoticeDetails  DCheckNotification
         }
     }
 }
